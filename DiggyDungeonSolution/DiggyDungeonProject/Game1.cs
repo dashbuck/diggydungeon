b75e9e9abbcf6c9a42a22f486682f8889917a000
Dash Buck <emdashbuck@gmail.com>
2024-01-06 17:53:58 -0800

    Title text where it should go!

--- DiggyDungeonSolution/DiggyDungeonProject/Game1.cs
+++ DiggyDungeonSolution/DiggyDungeonProject/Game1.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DiggyDungeonProject
 {
     public class Game1 : Game
     {
+        SpriteFont fontRubik;
+        SpriteFont fontDidthis;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -19,6 +24,9 @@ namespace DiggyDungeonProject
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -26,7 +34,7 @@ namespace DiggyDungeonProject
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            fontRubik = Content.Load<SpriteFont>("RubikDoodleShadow");
             // TODO: use this.Content to load your game content here
         }
 
@@ -35,6 +43,8 @@ namespace DiggyDungeonProject
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            
+
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -42,9 +52,53 @@ namespace DiggyDungeonProject
 
         protected override void Draw(GameTime gameTime)
         {
+            int windowWidth = _graphics.PreferredBackBufferWidth;
+            int windowHeight = _graphics.PreferredBackBufferHeight;
+
+            int posTopY = 2; //not 0 because we need a lil buffer
+            int posCenterX = windowWidth / 2; //I'd use a float for accuracy but I think V2s only accept ints
+            int posCenterY = windowHeight / 2;
+
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            
+            _spriteBatch.Begin();
+
+            //== Title ==
+
+            string titleText = "Diggy Dungeon";
+            Vector2 titleTextMeas = fontRubik.MeasureString(titleText);
+            Vector2 titleTextMidPoint = titleTextMeas / 2;
+            _spriteBatch.DrawString(
+                fontRubik, //SpriteFont spriteFont
+                titleText, //StringBuilder text
+                new Vector2(posCenterX, titleTextMidPoint.Y / 2), //Vector2 position - at top but not outside frame
+                Color.Black, //Color color
+                0, //float rotation
+                titleTextMidPoint, //Vector2 origin
+                0.6f, //Vector2 scale
+                SpriteEffects.None, //SpriteEffects effects
+                0 //float layerDepth
+                );
+
+            //== Subtitle ==
+            string titleTextSub = "Concentration";
+            Vector2 titleTextSubMeas = fontRubik.MeasureString(titleTextSub);
+            Vector2 titleTextSubMidPoint = titleTextSubMeas / 2;
+            _spriteBatch.DrawString(
+                fontRubik, //SpriteFont spriteFont
+                titleTextSub, //StringBuilder text
+                new Vector2(posCenterX, posTopY + titleTextMeas.Y ), //Vector2 position - below the title text.
+                Color.Black, //Color color
+                0, //float rotation
+                titleTextMidPoint, //Vector2 origin
+                1.15f, //Vector2 scale 
+                SpriteEffects.None, //SpriteEffects effects
+                0 //float layerDepth
+                );
+
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
--- DiggyDungeonSolution/DiggyDungeonProject/obj/Debug/net6.0/DiggyDungeonProject.AssemblyInfo.cs
+++ DiggyDungeonSolution/DiggyDungeonProject/obj/Debug/net6.0/DiggyDungeonProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiggyDungeonProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79fbd0bdbbdfcd53a430dc0a247af15e910baa56")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiggyDungeonProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiggyDungeonProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
